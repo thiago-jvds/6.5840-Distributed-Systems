6c3d4589f715c5b585128614dd9075f42599fb2d
thiago-jvds <thiago.jvds@gmail.com>
2025-02-19 16:54:25 -0500

    Lab 2: Done

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -10,8 +11,13 @@ type Lock struct {
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
 	ck kvtest.IKVClerk
-	// You may add code here
+	// Special key in the server for the lock
+	lkey string
+	// Lock Client Unique identifier
+	uId string
 }
+
+const DefaultLockOwner string = ""
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
 // perform a Put or Get by calling lk.ck.Put() or lk.ck.Get().
@@ -19,15 +25,115 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{ck: ck, lkey: l, uId: kvtest.RandValue(8)}
+
+	// If key doesn't exist, try until it exists
+	_, _, err := lk.ck.Get(lk.lkey)
+	if err == rpc.ErrNoKey {
+		for {
+			err = lk.ck.Put(lk.lkey, DefaultLockOwner, 0)
+			if err != rpc.ErrNoKey {
+				break
+			}
+		}
+	}
+
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	for {
+
+		currentHolder, version, err := lk.ck.Get(lk.lkey)
+
+		// fmt.Printf("Acquire, current %v, holder %v, version %v, err %v\n", lk.uId, currentHolder, version, err)
+
+		// If currentHolder is Requester
+		if currentHolder == lk.uId {
+			break
+		}
+
+		// Err in getting info about lock owner, or ownwership is taken
+		if err != rpc.OK || currentHolder != DefaultLockOwner {
+			continue
+		}
+
+		// If need to acquire the lock
+		err = lk.ck.Put(lk.lkey, lk.uId, version)
+		// fmt.Printf("Acquire inside, current %v, holder %v, version %v, err %v\n", lk.uId, currentHolder, version, err)
+
+		// If able to acquire the lock, break it off the loop
+		if err == rpc.OK {
+			break
+
+			// If any error other than Maybe, retry
+		} else if err != rpc.ErrMaybe {
+			continue
+
+			// If Maybe, check if any errors
+		} else if err == rpc.ErrMaybe {
+
+			for {
+				holder, _, err := lk.ck.Get(lk.lkey)
+
+				if err == rpc.OK {
+
+					// Put was succesful
+					if holder == lk.uId {
+						return
+					} else {
+						break
+					}
+
+				}
+			}
+
+		}
+
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	for {
+
+		_, version, err := lk.ck.Get(lk.lkey)
+
+		// fmt.Printf("Release, current %v, holder %v, version %v, err %v\n", lk.uId, currentHolder, version, err)
+
+		if err != rpc.OK {
+			continue
+		}
+
+		err = lk.ck.Put(lk.lkey, DefaultLockOwner, version)
+
+		// fmt.Printf("Release after, current %v, holder %v, version %v, err %v\n", lk.uId, currentHolder, version, err)
+
+		if err == rpc.OK {
+			break
+
+			// If any error other than Maybe, retry
+		} else if err != rpc.ErrMaybe {
+			continue
+
+			// If Maybe, check if any errors
+		} else if err == rpc.ErrMaybe {
+
+			for {
+				holder, _, err := lk.ck.Get(lk.lkey)
+
+				if err == rpc.OK {
+
+					// Put was succesful
+					if holder != lk.uId {
+						return
+					} else {
+						break
+					}
+
+				}
+			}
+
+		}
+
+	}
 }
