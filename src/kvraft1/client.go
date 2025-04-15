260967ee6204a64c0b4c4bc1feefc173ea7d491d
thiago-jvds <thiago.jvds@gmail.com>
2025-04-15 16:45:25 -0400

    Lab 4B: Done

--- src/kvraft1/client.go
+++ src/kvraft1/client.go
@@ -1,21 +1,25 @@
 package kvraft
 
 import (
-	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
-)
+	"time"
 
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
+)
 
 type Clerk struct {
 	clnt    *tester.Clnt
 	servers []string
 	// You will have to modify this struct.
+	lastLeader int
 }
 
 func MakeClerk(clnt *tester.Clnt, servers []string) kvtest.IKVClerk {
 	ck := &Clerk{clnt: clnt, servers: servers}
 	// You'll have to add code here.
+	ck.lastLeader = 0
+
 	return ck
 }
 
@@ -31,8 +35,32 @@ func MakeClerk(clnt *tester.Clnt, servers []string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 
-	// You will have to modify this function.
-	return "", 0, ""
+	// Keep trying indefintely
+	args := rpc.GetArgs{Key: key}
+
+	chosenIdx := ck.lastLeader
+	for {
+
+		DPrintf("Clerk: calling GET at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+		reply := rpc.GetReply{}
+
+		ok := ck.clnt.Call(ck.servers[chosenIdx%len(ck.servers)], "KVServer.Get", &args, &reply)
+
+		if ok && reply.Err == rpc.ErrNoKey {
+			DPrintf("Clerk: GET returned NoKey at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+			return "", 0, rpc.ErrNoKey
+		}
+
+		if ok && reply.Err == rpc.OK {
+			DPrintf("Clerk: GET returned OK at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+			ck.lastLeader = chosenIdx % len(ck.servers)
+			return reply.Value, reply.Version, reply.Err
+		}
+
+		chosenIdx++
+		time.Sleep(10 * time.Millisecond)
+
+	}
 }
 
 // Put updates key with value only if the version in the
@@ -53,6 +81,55 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
-	// You will have to modify this function.
-	return ""
+
+	args := rpc.PutArgs{
+		Key:     key,
+		Value:   value,
+		Version: version,
+	}
+
+	// defaults to 'false'
+	hasGoneTru := make([]bool, len(ck.servers))
+	chosenIdx := ck.lastLeader
+	for {
+
+		reply := rpc.PutReply{}
+		DPrintf("Clerk: calling PUT at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+
+		ok := ck.clnt.Call(ck.servers[chosenIdx%len(ck.servers)], "KVServer.Put", &args, &reply)
+
+		// First time trying
+		if ok && !hasGoneTru[chosenIdx%len(ck.servers)] && reply.Err == rpc.ErrVersion {
+			DPrintf("Clerk: PUT returned ErrVersion at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+
+			// ck.lastLeader = chosenIdx % len(ck.servers)
+
+			return rpc.ErrVersion
+		}
+
+		hasGoneTru[chosenIdx%len(ck.servers)] = true
+
+		if ok && reply.Err == rpc.ErrNoKey {
+			DPrintf("Clerk: PUT returned noKey at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+
+			return rpc.ErrNoKey
+		}
+
+		if ok && reply.Err == rpc.OK {
+			DPrintf("Clerk: PUT returned OK at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+			ck.lastLeader = chosenIdx % len(ck.servers)
+
+			return rpc.OK
+		}
+
+		if ok && reply.Err == rpc.ErrVersion {
+			DPrintf("Clerk: PUT returned Maybe at %s\n", ck.servers[chosenIdx%len(ck.servers)])
+
+			return rpc.ErrMaybe
+		}
+
+		chosenIdx++
+		time.Sleep(10 * time.Millisecond)
+
+	}
 }
--- src/kvraft1/server.go
+++ src/kvraft1/server.go
@@ -1,22 +1,29 @@
 package kvraft
 
 import (
+	"sync"
 	"sync/atomic"
 
 	"6.5840/kvraft1/rsm"
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labgob"
 	"6.5840/labrpc"
-	"6.5840/tester1"
-
+	tester "6.5840/tester1"
 )
 
 type KVServer struct {
+	mu   sync.Mutex
 	me   int
 	dead int32 // set by Kill()
 	rsm  *rsm.RSM
 
 	// Your definitions here.
+	kvdbase map[any]KDBEntry
+}
+
+type KDBEntry struct {
+	Value   string
+	Version rpc.Tversion
 }
 
 // To type-cast req to the right type, take a look at Go's type switches or type
@@ -25,8 +32,59 @@ type KVServer struct {
 // https://go.dev/tour/methods/16
 // https://go.dev/tour/methods/15
 func (kv *KVServer) DoOp(req any) any {
-	// Your code here
-	return nil
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	switch req := req.(type) {
+
+	case rpc.GetArgs:
+		key := req.Key
+		p, ok := kv.kvdbase[key]
+
+		reply := rpc.GetReply{}
+
+		if !ok {
+			reply.Err = rpc.ErrNoKey
+			return &reply
+		}
+
+		reply.Err = rpc.OK
+		reply.Value = p.Value
+		reply.Version = p.Version
+
+		return &reply
+
+	case rpc.PutArgs:
+		key := req.Key
+		val := req.Value
+		proposedVersion := req.Version
+
+		reply := rpc.PutReply{}
+
+		p, ok := kv.kvdbase[key]
+
+		var v rpc.Tversion
+		if !ok {
+			v = 0
+		} else {
+			v = p.Version
+		}
+
+		if proposedVersion != v {
+			reply.Err = rpc.ErrVersion
+			return &reply
+		}
+
+		kv.kvdbase[key] = KDBEntry{val, proposedVersion + 1}
+
+		reply.Err = rpc.OK
+		return &reply
+
+	default:
+		DPrintf("KVServer %v: unknown request type %T\n", kv.me, req)
+		return nil
+	}
+
 }
 
 func (kv *KVServer) Snapshot() []byte {
@@ -42,12 +100,63 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here. Use kv.rsm.Submit() to submit args
 	// You can use go's type casts to turn the any return value
 	// of Submit() into a GetReply: rep.(rpc.GetReply)
+
+	kv.mu.Lock()
+	if _, isLeader := kv.rsm.Raft().GetState(); !isLeader {
+		reply.Err = rpc.ErrWrongLeader
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+
+	DPrintf("KVServer %v: GET called with args %v\n", kv.me, args)
+
+	err, val := kv.rsm.Submit(*args)
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	DPrintf("val: %v\n", val)
+
+	if err == rpc.OK {
+		reply_ := val.(*rpc.GetReply)
+		reply.Err = reply_.Err
+		reply.Value = reply_.Value
+		reply.Version = reply_.Version
+		DPrintf("KVServer %v: GET replied OK with reply %v\n", kv.me, reply)
+	} else {
+		reply.Err = err
+		DPrintf("KVServer %v: GET replied with an error with reply %v\n", kv.me, reply)
+	}
+
 }
 
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here. Use kv.rsm.Submit() to submit args
 	// You can use go's type casts to turn the any return value
 	// of Submit() into a PutReply: rep.(rpc.PutReply)
+
+	if _, isLeader := kv.rsm.Raft().GetState(); !isLeader {
+		reply.Err = rpc.ErrWrongLeader
+		return
+	}
+	DPrintf("KVServer %v: PUT called with args %v\n", kv.me, args)
+
+	err, val := kv.rsm.Submit(*args)
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	DPrintf("val: %v\n", val)
+
+	if err == rpc.OK {
+		reply_ := val.(*rpc.PutReply)
+		reply.Err = reply_.Err
+	} else {
+		reply.Err = err
+	}
+
+	DPrintf("KVServer %v: PUT called returned %v\n", kv.me, reply)
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -79,8 +188,8 @@ func StartKVServer(servers []*labrpc.ClientEnd, gid tester.Tgid, me int, persist
 
 	kv := &KVServer{me: me}
 
-
 	kv.rsm = rsm.MakeRSM(servers, me, persister, maxraftstate, kv)
 	// You may need initialization code here.
+	kv.kvdbase = make(map[any]KDBEntry)
 	return []tester.IService{kv, kv.rsm.Raft()}
 }
